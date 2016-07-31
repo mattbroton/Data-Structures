983c6ebb3c718a89a51c335d09d46ffccec142f6
mattbroton <mattbroton@gmail.com>
2016-07-31 12:16:29 -0500

    Initial Commit

---
+++ Data Structures/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Data Structures
+//
+//  Created by Matthew Broton on 7/31/16.
+//  Copyright © 2016 Matthew Broton. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
